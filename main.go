81b4e887889247718ba43de2e6fdd4c7395a57ab
Dharma Chandra Viriya <dharmachandraviriya@Dharmas-MacBook-Pro.local>
2026-05-25 22:42:28 +0700

    membuat loop action ketika pilih aksi kembali pada nomor

--- main.go
+++ main.go
@@ -9,22 +9,18 @@ import (
 
 func main() {
 	utils.Migrate()
-	var action int
-	fmt.Println("---------- BangunIn ---------- ")
-	utils.PrintCustom(&action, []string{"Bahan Bangunan", "Supplier", "Keluar"})
-	switch action {
-	case 1:
-		var action_bangunan int
-		fmt.Println("---------- Bahan Bangunan ---------- ")
-		utils.PrintCRUD(&action_bangunan)
-		bahanBangunan.Index(action_bangunan)
-	case 2:
-		var action_supplier int
-		fmt.Println("---------- Supplier ---------- ")
-		utils.PrintCRUD(&action_supplier)
-		supplier.Index()
-	case 3:
-		fmt.Println("Keluar!")
-		return
+	for {
+		fmt.Println("---------- BangunIn ---------- ")
+		var action int
+		utils.PrintCustom(&action, []string{"Bahan Bangunan", "Supplier", "Keluar"})
+		switch action {
+		case 1:
+			bahanBangunan.Index()
+		case 2:
+			supplier.Index()
+		case 3:
+			fmt.Println("Keluar!")
+			return
+		}
 	}
 }
