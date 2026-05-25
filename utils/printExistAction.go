e358ee1beddcf9055ca742fd3b2f104faed58d3d
Dharma Chandra Viriya <dharmachandraviriya@Dharmas-MacBook-Pro.local>
2026-05-25 19:36:32 +0700

    setup users model, and print utils

---
+++ utils/printExistAction.go
@@ -0,0 +1,27 @@
+package utils
+
+import "fmt"
+
+func PrintCRUD(action_var *int) {
+	var labels = []string{"Read", "Create", "Update", "Delete"}
+	for i, label := range labels {
+		fmt.Printf("[%d] %s\n", i+1, label)
+	}
+	fmt.Print("Masukan aksi: ")
+	fmt.Scanln(action_var)
+	if *action_var < 1 || *action_var > len(labels) {
+		fmt.Println("Aksi tidak valid")
+		PrintCRUD(action_var)
+	}
+}
+func PrintCustom(action_var *int, labels []string) {
+	for i, label := range labels {
+		fmt.Printf("[%d] %s\n", i+1, label)
+	}
+	fmt.Print("Masukan aksi: ")
+	fmt.Scanln(action_var)
+	if *action_var < 1 || *action_var > len(labels) {
+		fmt.Println("Aksi tidak valid")
+		PrintCustom(action_var, labels)
+	}
+}
